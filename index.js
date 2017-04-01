302ed18ca855065150771f3138cff832ba2984b0
Prataik <pratikm279@gmail.com>
2017-04-01 14:51:39 +0530

    adding routes

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const routes = require('./routes/api');
+const bodyParser = require('body-parser');
 
 //setup an express app
 const app = express();
 
+//middleware to parse request-body
+app.use(bodyParser.json());
+
+//Initialize routes
+app.use(routes);
+
 //Listen for requests
 //process.env.port : port that is set up on particular env like Heroku
 app.listen(process.env.port || 4000, function(){
-  console.log('Listening to port 4000');
+  console.log('Heyyy Listening to port 4000');
 })
