c700f93bb9aa2e55395d9bc00d5504a4c8ee2184
Prataik <pratikm279@gmail.com>
2017-04-02 20:49:44 +0530

    adding connection to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const routes = require('./routes/api');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 //setup an express app
 const app = express();
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/ridego');
+mongoose.Promise = global.Promise;                     //mongoose.Promise is deprecated
 
 //middleware to parse request-body
 app.use(bodyParser.json());
