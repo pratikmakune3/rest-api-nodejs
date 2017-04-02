836ee0e3a67fa06b38ecd8b67ca9193bd889ea73
Prataik <pratikm279@gmail.com>
2017-04-02 20:53:21 +0530

    Modifying the POST handler

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Ride = require('../models/ride');
 
 //Retrieves the Rides
 router.get('/api/rides', function(req,res){
@@ -7,13 +8,13 @@ router.get('/api/rides', function(req,res){
   res.send({type : 'GET'});
 });
 
-//Adds the Ride
+//Adds the Ride to db
 router.post('/api/rides', function(req,res){
-  console.log(req.body);
-  res.send({
-            type : 'POST',
-            name : req.body.name
-          });
+  //mongoose method create return a Promise...
+  // Create a ride locally by passing user data in POST and Save it to the db
+  Ride.create(req.body).then(function(ride){
+    res.send(ride);
+  });
 });
 
 //Updates the Ride
